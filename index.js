56a87dfb8285843cb28bf2ff93fbcb4d381eeccc
Maauricioo <mauriciosilvasouza33@gmail.com>
2019-10-03 11:16:59 -0300

    Add tela agenda

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './App';
 import {name as appName} from './app.json';
+import agenda from './src/screens/agenda'
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => agenda);
